2794d21d4f650ff5667fca5a33bcaf1b3d7edf8d
Tony Jiang <yujiang99@gmail.com>
2022-06-14 16:08:53 -0700

    It works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,13 +58,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:creator_id, :post_text, :context_type, :pinned)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:creator_id, :post_text, :context_type, :pinned)
+  end
 end
