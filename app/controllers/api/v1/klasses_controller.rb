e77b9330ac90c4454cf63716ca190789b75b3370
Tony Jiang <yujiang99@gmail.com>
2022-07-21 01:43:50 -0700

    Klasses controller

---
+++ app/controllers/api/v1/klasses_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::KlassesController < ApiController
+  before_action :set_klass, only: %i[show update destroy]
+
+  # GET /klasses or /klasses.json
+  def index
+    @klasses = Klass.where(owner_id: params['user_id'])
+    render json: @klasses
+  end
+
+  # GET /klasses/1 or /klasses/1.json
+  def show
+    render json: @klass
+  end
+
+  # POST /klasses or /klasses.json
+  def create
+    @klass = Klass.new(klass_params)
+
+    if @klass.save
+      render json: @klass, status: :created
+    else
+      render json: @klass.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /klasses/1 or /klasses/1.json
+  def update
+    if @klass.update(klass_params)
+      render json: @klass, status: :ok
+    else
+      render json: @klass.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /klasses/1 or /klasses/1.json
+  def destroy
+    @klass.destroy
+    head :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_klass
+    @klass = Klass.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def klass_params
+    params.require(:klass).permit(:course_id, :creator_id, :owner_id, :start_date)
+  end
+end
