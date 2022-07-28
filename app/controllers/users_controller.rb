bcd264e5f512378977c66590fd92f3863889bf84
Tony Jiang <yujiang99@gmail.com>
2022-07-28 08:13:02 -0700

    Remove fixtures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,7 @@ class UsersController < ApplicationController
   # before_action :authenticate_user!
 
   def home
-    app = Doorkeeper::Application.find_by(name: 'Web client')
-
-    @application = {
-      name: app.name,
-      client_id: app.uid,
-      client_secret: app.secret
-    }
+    render json: 'Hello World!'
   end
 
   def myposts
