404307e24a5554fbab84e908c5c843558e9fb35b
GiuseppeIII <josephbabbo1@gmail.com>
2022-10-12 00:48:20 -0700

    persist

--- app/controllers/api/v1/course_memberships_controller.rb
+++ app/controllers/api/v1/course_memberships_controller.rb
@@ -1,9 +1,9 @@
-class Api::V1::CourseMembershipsController < ApplicationController
+class Api::V1::CourseMembershipsController < ApiController
   before_action :set_course_membership, only: %i[ show edit update destroy ]
 
   # GET /api/v1/course_memberships or /api/v1/course_memberships.json
   def index
-    user = User.find(params['user_id'])
+    user = User.find(params[:user_id])
     render json: user.course_memberships.select(('*')).order(order: :asc).uniq
   end
 
@@ -11,21 +11,35 @@ class Api::V1::CourseMembershipsController < ApplicationController
   def create
     @course_membership = CourseMembership.new(course_membership_params)
 
-    if @course.save
-      render json: @course, status: :created
+    if @course_membership.save
+      render json: @course_membership, status: :created
     else
-      render json: @course.errors, status: :unprocessable_entity
+      render json: @course_membership.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/course_memberships/1 or /api/v1/course_memberships/1.json
   def update
-    logger.debug(params)
-    # if @course_membership.update(course_membership_params)
-    #   render json: @course_membership, status: :ok
-    # else
-    #   render json: @course_membership.errors, status: :unprocessable_entity
-    # end
+    source = params[:source]
+    destination = params[:destination]
+    if source && destination
+      if source > destination
+        user = User.find(params[:user_id])
+        user.course_memberships.order(order: :asc).limit((source - destination).abs()).offset(destination + 1).update_all("'order' = 'order' + 1")
+        logger.debug("add 1 to order from destination to source - 1")
+      elsif source < destination
+        user = User.find(params[:user_id])
+        user.course_memberships.order(order: :asc).limit((source - destination).abs()).offset(source + 1).update_all("'order' = 'order' - 1")
+        logger.debug("subtract from order from source to destination - 1")
+      end
+    end
+
+    if @course_membership.update(course_membership_params)
+      render json: @course_membership, status: :ok
+      logger.debug("uppppppdate")
+    else
+      render json: @course_membership.errors, status: :unprocessable_entity
+    end
   end
 
   # DELETE /api/v1/course_memberships/1 or /api/v1/course_memberships/1.json
@@ -46,8 +60,11 @@ class Api::V1::CourseMembershipsController < ApplicationController
         :course,
         :user,
         :order,
+        :source,
+        :destination,
         :academic_role,
-        :user_role
+        :user_role,
+        :user_id
       )
     end
 end
