08809ca6b698cc2b29d67ac600e74febd15eaec5
Tony Jiang <86689874+hippopotony@users.noreply.github.com>
2023-01-17 00:36:07 -0800

    Add attachments for syllabus

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -1,3 +1,14 @@
 class Syllabus < ApplicationRecord
   belongs_to :course
+
+  has_many_attached :uploads
+
+  def uploads_data
+    uploads.map do |upload|
+      {
+        name: upload.blob.filename.to_s,
+        url: Rails.application.routes.url_helpers.url_for(upload)
+      }
+    end
+  end
 end
