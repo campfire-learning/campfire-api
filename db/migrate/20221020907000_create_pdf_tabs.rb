31c7d5048755a49276a1a5957827ee4d007b6fae
Tony Jiang <86689874+hippopotony@users.noreply.github.com>
2023-02-01 21:35:42 -0800

    Delete tabs

--- db/migrate/20221020907000_create_pdf_tabs.rb
+++
@@ -1,8 +0,0 @@
-class CreatePdfTabs < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pdf_tabs do |t|
-      #actual pdf stored in active_storage
-      t.timestamps null: false
-    end
-  end
-end
